bac075d8d147fac2dd459126c233de48d6e1640b
LorenzoNicco <lorenzoniccolai91@gmail.com>
2023-02-08 17:47:45 +0100

    Add ms 2/ conversione lingua film in bandiera

--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -15,9 +15,15 @@ export default {
     <ul v-if="store.loaded == true">
         <li>Titolo: {{ store.movies.results[0].title }}</li>
         <li>Titolo originale: {{ store.movies.results[0].original_title }}</li>
-        <li>Lingua: {{ store.movies.results[0].original_language }}</li>
+        <!-- inizio sezione lingua -->
+        <li v-if="store.movies.results[0].original_language == 'en'">Lingua:  <span class="fi fi-gb"></span></li>
+        <li v-else>Lingua:  <span :class="'fi fi-'+store.movies.results[0].original_language"></span></li>
+        <!-- fine sezione lingua -->
         <li>Voto: {{ store.movies.results[0].vote_average }}</li>
     </ul>
+
+    <!-- collegamento cdn per bandiere -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lipis/flag-icons@6.6.6/css/flag-icons.min.css"/>
 </template>
 
 <style scoped>
